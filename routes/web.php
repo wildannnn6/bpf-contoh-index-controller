0fbdc6441c1f30592878af45e647d016cbd5e8f7
wildan_2tib <wildan24ti@mahasiswa.pcr.ac.id>
2025-09-23 08:50:25 +0700

    commit awal

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+})->name('mahasiswa.show');
+
+route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
